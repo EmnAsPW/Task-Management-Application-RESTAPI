7decd3f70d87c50a83d4807dc1a459cf516e568c
EmnAsPW <emonsourov@gmail.com>
2023-10-11 10:50:55 +0600

    third commit

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './tasks.model';
 
 @Injectable()
 export class TasksService {
